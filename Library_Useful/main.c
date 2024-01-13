b6862f458a02d3a7ccb98f9f71d9a87e893d3963
gangueboris <gangueboris@gmail.com>
2024-01-13 20:55:46 +0100

    new

--- Library_Useful/main.c
+++ Library_Useful/main.c
@@ -7,8 +7,6 @@
 hash dict[N];
 
 int main(){
-   //int a = get_hash("22d");
-   // printf("the %d",a);
    char* b = "22ddd";
    char* c = "dd2d2";
    char* d = "LeetCode";
@@ -21,25 +19,19 @@ int main(){
    add(b,d); 
    add(c,"Hello_world");// h of bis equal to h of c
   
-
-   
    for(int i = 0; i < N; i++){
    printf("key: %s  value %s\n",dict[i].tuple[i][0], dict[i].tuple[i][1]);    
    }
-   //get_free();
-   printf("YHJe value of h is :%d\n",h); 
-   // e = dict[h].tuple[h][1];
   
+   printf("YHJe value of h is :%d\n",h); 
+
    e = get(c);
    printf("I got the value: %s\n",e);
    
    for(int i = 0; i < N; i++){
-   //printf("key: %s  value %s\n",dict[i].tuple[i][0], dict[i].tuple[i][1]);   
-     printf("key: %s  value %s\n",dict[i].tuple[i][0], dict[i].tuple[i][1]);
+       printf("key: %s  value %s\n",dict[i].tuple[i][0], dict[i].tuple[i][1]);
    }
    del(d);
-
-
 
    get_free();
   
